2b75b071a6ce4cc349cefbfb62a4b392f7cfdd71
Willis Erdman <willis.erdman@gmail.com>
2021-11-06 22:09:36 -0700

    got first person camera rotation working, rotation math is hard

--- games/trying3js/src/EventHandler.js
+++ games/trying3js/src/EventHandler.js
@@ -5,7 +5,35 @@ export default class EventHandler {
         document.addEventListener("mousemove", (e) => {
             this.mousePosition.changeX = e.movementX;
             this.mousePosition.changeY = e.movementY;
+            console.log(this.mousePosition)
         }, false);
+
+        this.movements = {}
+        document.addEventListener("keydown", (e) => {
+            switch (e.key) {
+                case "w":
+                case "ArrowUp":
+                    this.movements.forward = true;
+                    break;
+                case "a":
+                case "ArrowLeft":
+                    this.movements.left = true;
+                    break;
+                case "s":
+                case "ArrowDown":
+                    this.movements.back = true;
+                    break;
+                case "d":
+                case "ArrowRight":
+                    this.movements.right = true;
+                    break;
+                case "q":
+                    this.movements.q = true;
+                    break;
+                case "e":
+                    this.movements.e = true;
+            } //switch
+        }) //event listener
 
         //this.one = 1;
     }
--- games/trying3js/src/index.js
+++ games/trying3js/src/index.js
@@ -1,6 +1,7 @@
 
 import * as THREE from "https://cdn.skypack.dev/three@0.134.0";
 import EventHandler from "./EventHandler.js";
+import GameEngine from "./GameEngine.js";
 
 const FOV = 75;
 const NEAR_BOUND = 0.1;
@@ -19,30 +20,44 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 let canvas = renderer.domElement;
-
 canvas.onclick = () => {
     canvas.requestPointerLock();
     canvas.requestFullscreen();
 }
 
-const geometry = new THREE.BoxGeometry(100, 2, 100);
+const baseGeometry = new THREE.BoxGeometry(100, 2, 100);
+const wallGeometry = new THREE.BoxGeometry(100, 10, 2)
+const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
 const green = new THREE.MeshBasicMaterial({color: 0x00ff00});
 const red = new THREE.MeshBasicMaterial({color: 0xff0000});
-const floor = new THREE.Mesh(geometry, green);
-const roof = new THREE.Mesh(geometry, red);
-roof.translateY(10);
+const blue = new THREE.MeshBasicMaterial({color: 0x0000ff});
+
+const floor = new THREE.Mesh(baseGeometry, green);
+const roof = new THREE.Mesh(baseGeometry, red);
+const wall = new THREE.Mesh(wallGeometry, blue);
+const wall2 = new THREE.Mesh(wallGeometry, blue);
+const box = new THREE.Mesh(boxGeometry, blue);
+
+roof.position.set(0, 10, 0)
+wall.position.set(0, 5, 50);
+wall2.position.set(0, 5, -50)
+
+scene.add(wall2);
+//scene.add(wall);
 scene.add(roof);
 scene.add(floor);
+scene.add(box);
 
-camera.position.z = 5;
-camera.position.x = 5;
+//camera.position.x = 5;
 camera.position.y = 5;
 
 let camBuffer, xBuffer, yBuffer;
 const events = new EventHandler();
 let clock = new THREE.Clock();
 let timeDelta;
+const game = new GameEngine();
 
+camera.rotation.set(0, 0, 0, "YXZ")
 function animate() {
 
     camBuffer = camera.rotation.y;
@@ -58,8 +73,6 @@ function animate() {
     yBuffer = events.mousePosition.changeY;
     if (camera.rotation.x >= Math.PI / 2) camera.rotation.x = Math.PI / 2;
     if (camera.rotation.x <= -Math.PI / 2) camera.rotation.x = -Math.PI / 2;
-
-    //console.log(camera.rotation);
 
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
