40405261b35bf0abe47b6158a7e5b942f7a00068
Willis Erdman <willis.erdman@gmail.com>
2021-12-21 16:38:07 -0800

    moved elsewhere, links will probably be broken

--- fun_stuff/trying3js/src/GameEngine.js
+++
@@ -1,9 +0,0 @@
-export default class GameEngine {
-    constructor() {}
-    update(camera, vectorPosition, eulerRotation) {
-        this.updateCamera(camera, vectorPosition, eulerRotation);
-    }
-    updateCamera() {
-
-    }
-}
