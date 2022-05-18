17e0cf91860ce25b3097ee3fbedf0261734fbee0
Willis Erdman <willis.erdman@gmail.com>
2022-05-17 18:46:23 -0700

    project

---
+++ CurrentProjects/TrivoryKnockoff/src/routes/index.svelte
@@ -0,0 +1,26 @@
+<script>
+	import 'bulma';
+</script>
+
+<div class="container is-max-desktop pb-0">
+	<div class="columns is-centered">
+		<div class="column is-one-quarter p-0">
+			<aside class="r-shadow hero menu is-size-5 has-text-weight-medium p-6 pb-0 is-fullheight">
+				<ul class="menu-list">
+					<li><a class="has-text-black" href="/">Home</a></li>
+					<li><a class="has-text-black" href="/news">News</a></li>
+					<li><a class="has-text-black" href="/calendar">Calendar</a></li>
+					<li><a class="has-text-black" href="/resources">Resources</a></li>
+					<li><a class="has-text-black pr-0" href="/directory">Staff Directory</a></li>
+				</ul>
+			</aside>
+		</div>
+		<div class="column is-three-quarters py-6">main content goes here</div>
+	</div>
+</div>
+
+<style>
+	.r-shadow {
+		box-shadow: 10px 0px 5px -8px #ccc;
+	}
+</style>
