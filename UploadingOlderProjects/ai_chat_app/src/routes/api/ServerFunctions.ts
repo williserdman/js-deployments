9d3846d6037db51aa95da4eab318c25af7f6a7cc
williserdman <willis.erdman@gmail.com>
2023-01-18 19:13:50 -0800

    Updated with API call now in main program.

--- UploadingOlderProjects/ai_chat_app/src/routes/api/ServerFunctions.ts
+++ UploadingOlderProjects/ai_chat_app/src/routes/api/ServerFunctions.ts
@@ -56,53 +56,35 @@ export const getClientInfo = async (userID: string) => {
 	return (await (await get(ref(db, "users/" + userID + "/clientInfo"))).val()) ?? "";
 };
 
-// While I don't have the API running alongside this server, these are real responses and are being used for testing.
-let counter = 0;
+import axios from "axios";
+
+import { API_KEY } from "$env/static/private";
+
 export const mockAI = (question: string): string => {
-	let rtrn: string;
-	switch (counter) {
-		case 0:
-			rtrn = "Yes, I do.";
-			break;
-		case 1:
-			rtrn = "About once a week.";
-			break;
-		case 2:
-			rtrn = "Occasionally, but usually just for special films.";
-			break;
-		case 3:
-			rtrn = "Mostly at home, on a streaming service or Blu-ray.";
-			break;
-		case 4:
-			rtrn = "I use Netflix and Hulu.";
-			break;
-		case 5:
-			rtrn = "Netflix starts at $8.99 per month and Hulu starts at $5.99 per month.";
-			break;
-		case 6:
-			rtrn = "I can afford to spend up to $200,000 on a home.";
-			break;
-		case 7:
-			rtrn =
-				"I am looking for a safe, family-friendly neighborhood with good acess to schools and amenities.";
-			break;
-		case 8:
-			rtrn = "I care about walkability a lot, so an 8 or higher.";
-			break;
-		case 9:
-			rtrn = "Yes, I need a garage.";
-			break;
-		case 10:
-			rtrn = "Yes, I have two children.";
-			break;
-		case 11:
-			rtrn =
-				"A realtor could show me homes that meet my needs and help me understand the value of each property. They could also provide data on potential perks or discounts I may be eligible for, such as grants or tax deductions.";
-			break;
-		default:
-			rtrn = "I'm not sure how to answer that question.";
-			break;
-	}
-	++counter;
-	return rtrn;
+	const GPT3_API = "https://api.openai.com/v1/engines/davinci-codex/completions";
+
+	const options = {
+		prompt: prompt,
+		max_tokens: 2048,
+		api_key: API_KEY
+	};
+
+	const request = {
+		method: "post",
+		url: GPT3_API,
+		data: options
+	};
+
+	let r: string = "";
+
+	axios(request)
+		.then((response) => {
+			r = response.data;
+			console.log(response.data);
+		})
+		.catch((error) => {
+			r = "ERROR";
+			console.error(error);
+		});
+	return r;
 };
